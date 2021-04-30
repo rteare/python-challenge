351894969895185f2de1d1eba17e015acf53dbc4
Ross Teare <rteare.7@gmail.com>
2021-04-30 18:57:16 -0400

    final commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,51 +4,73 @@ import csv
 #Path to collect data from Resources folder
 PyPoll_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
 
+# List of Variables
+votes = []
+canditates = []
+total_votes = 0
+khan = []
+khan_votes = 0
+khan_percent = 0
+correy = []
+correy_votes = 0
+correy_percent = 0
+li = []
+li_votes = 0
+li_percent = 0
+otooley = []
+otooley_votes = 0
+otooley_percent = 0
+
+
 # Reading CSV file
 with open(PyPoll_csv) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # zip the csv data into tuples to better analyze
-    election_data = list(zip(*csvreader))
+    # Read the header row first
+    csv_header = next(csvreader)
+    
+    # Total votes
+    for vote in csvreader:
+        votes.append(vote[0])
+        canditates.append(vote[2])
+        total_votes = len(votes)
 
-    # define column data starting point 
-    voter_id_column = election_data[0][1:]
-    county_column = election_data[1][1:]
-    candidate_column = election_data[2][1:]
+    # Total votes per candidate
+    for candidate in canditates:
+        if candidate == "Khan":
+            khan.append(candidate)
+            khan_votes = len(khan)
+        
+        elif candidate == "Correy":
+            correy.append(candidate)
+            correy_votes = len(correy)
+        
+        elif candidate == "Li":
+            li.append(candidate)
+            li_votes = len(li)
 
-    # counting the amout of rows in to determin the total votes in the election
-    total_votes = len(voter_id_column)
+        else:
+            otooley.append(candidate)
+            otooley_votes = len(otooley)
+    
+    # Percentage of Vote
+    khan_percent = round((khan_votes / total_votes)*100, 3)
+    correy_percent = round((correy_votes / total_votes)*100, 3)
+    li_percent = round((li_votes / total_votes)*100, 3)
+    otooley_percent = round((otooley_votes / total_votes)*100, 3)
 
-    # find list of candidates in the election
-    candidates = set(candidate_column)
-
-    # define data variable for dictionary
-    data = {}
-
-    # for loop to find candidate data
-    for candidate in candidates:
-        votes = len([x for x in candidate_column if x == candidate])
-
-        # percentage of vote calculation
-        percentage_vote = 100 * votes / total_votes
-
-        # dicitionary components
-        data[votes] = (candidate, percentage_vote)
-
-        # define results by creating a list
-        results = list(data.keys())
-
-        #sort the votes
-        results.sort(reverse=True)
-
-
-    # declair the victor
-    winner_key = max(results)
-
-    # declair winner name
-    winner = data[winner_key][0]  
+    # Determine highest vote count
+    if khan_votes > max(correy_votes, li_votes, otooley_votes):
+        election_winner = "Khan"
+    elif correy_votes > max(khan_votes, li_votes, otooley_votes):
+        election_winner = "Correy"
+    elif li_votes > max(khan_votes, correy_votes, otooley_votes):
+        election_winner = "Li"
+    else:
+        election_winner = "O'Tooley"
+    
                
     # Print out Election Results
     print(f" ")
@@ -56,16 +78,12 @@ with open(PyPoll_csv) as csvfile:
     print(f"-------------------------")
     print(f"Total Votes: {total_votes}")
     print(f"-------------------------")
-    #
-    for key in results:
-            value = data[key]
-            politician = value[0]
-            percent_votes = value[1]
-            number_votes = key
-
-            print(f"{politician}: {percent_votes:.3f}% ({number_votes})")#print(f"{politician}: {percent_votes:.3f}% ({number_votes})")
+    print(f"Khan: {khan_percent:.3f}% {khan_votes}")
+    print(f"Correy: {correy_percent:.3f}% {correy_votes}")
+    print(f"Li: {li_percent:.3f}% {li_votes}")
+    print(f"O'Tooley: {otooley_percent:.3f}% {otooley_votes}")
     print(f"-------------------------")
-    print(f"Winner: {winner}")
+    print(f"Winner: {election_winner}")
     print(f"-------------------------")
 
     # Export to Text File
@@ -78,7 +96,10 @@ with open(PyPoll_csv) as csvfile:
         txt.write(f"----------------------------\n")
         txt.write(f"Total Votes: {total_votes}\n")
         txt.write(f"----------------------------\n")
-        txt.write(f"{politician}: {percent_votes:.3f}% ({number_votes})\n")
+        txt.write(f"Khan: {khan_percent:.3f}% {khan_votes}\n")
+        txt.write(f"Correy: {correy_percent:.3f}% {correy_votes}\n")
+        txt.write(f"Li: {li_percent:.3f}% {li_votes}\n")
+        txt.write(f"O'Tooley: {otooley_percent:.3f}% {otooley_votes}\n")
         txt.write(f"----------------------------\n")
-        txt.write(f"Winner: {winner}\n")
+        txt.write(f"Winner: {election_winner}\n")
         txt.write(f"----------------------------\n")
